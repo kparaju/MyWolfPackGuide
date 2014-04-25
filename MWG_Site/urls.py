c72bb7f4100db95854eb642a522e558830f08981
Kshitij Parajuli <kshitijparajuli@gmail.com>
2014-04-24 21:40:24 -0400

    Pictures work

--- MWG_Site/urls.py
+++ MWG_Site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from MWG_Site import views
+from MyWolfpackGuide import settings
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
@@ -11,4 +12,10 @@ urlpatterns = patterns('',
     url(r'^events/create/(?P<pk>\w+)', views.CreateEvent.as_view(), name='create-event'),
     url(r'^events/details/(?P<pk>\w+)', views.EventDetails.as_view(), name='event-details'),
 )
+
+##### do not do this in production
+if settings.DEBUG:
+	urlpatterns += patterns('',
+	        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+	        'document_root': settings.MEDIA_ROOT}))
 
